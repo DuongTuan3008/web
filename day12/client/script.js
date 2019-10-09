b9b0c3ad89bd3c06e88ed35d43c6c45b4e58e0b9
Tuan Duong <duongqtuan3008@gmail.com>
2019-10-09 21:43:12 +0700

    fixed

--- day12/client/script.js
+++ day12/client/script.js
@@ -60,7 +60,7 @@ $(document).ready(function () {
             <div class="col-12 mt-3 mb-3">
               <div class="row">
                 <div class="col-3">
-                  <img class="image-placeholder" src="https://images2.minutemediacdn.com/image/upload/c_crop,h_1193,w_2121,x_0,y_64/f_auto,q_auto,w_1100/v1565279671/shape/mentalfloss/578211-gettyimages-542930526.jpg"></img>
+                  <img class="image-placeholder" src="https://www.adazing.com/wp-content/uploads/2019/02/open-book-clipart-03.png"></img>
                 </div>
                 <div class="col-9">
                   <h3 class="text-success">${data.data[i].name}</h3>
